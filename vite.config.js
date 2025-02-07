d5c16453feebae44ecd71c77747c33d5cd1f756b
brthhule <brennenle@yahoo.com>
2025-02-07 11:33:15 -0500

    Continuing trying to make deploy work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/umarylandigem.github.io',
   plugins: [react()],
+  base: '/umarylandigem.github.io/'
 })
